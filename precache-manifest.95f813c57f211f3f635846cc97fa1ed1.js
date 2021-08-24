c056cf0ba5ad6a04f0ec88419f78eb0caee70f7d
chiawei913 <80014503+chiawei913@users.noreply.github.com>
2021-08-24 07:19:52 +0000

    Deploying to gh-pages from @ chiawei913/genshin_front@479b0789cd7c2e26b18c6b94461d331d9de85266 🚀

---
+++ precache-manifest.95f813c57f211f3f635846cc97fa1ed1.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2510a21eaa619aa51a",
+    "url": "css/Attribute.2a3a06c5.css"
+  },
+  {
+    "revision": "0fd170081c9de735a537",
+    "url": "css/BackAccount.2a3a06c5.css"
+  },
+  {
+    "revision": "a67df409837b5b2657d5",
+    "url": "css/admin.2a3a06c5.css"
+  },
+  {
+    "revision": "fddc19183e96a277c660",
+    "url": "css/app.b1cf5d92.css"
+  },
+  {
+    "revision": "8e5d48cbc9e66b0a4375",
+    "url": "css/character.2a3a06c5.css"
+  },
+  {
+    "revision": "5840554d5e90781efd44",
+    "url": "css/chunk-vendors.86c3935d.css"
+  },
+  {
+    "revision": "36a19b0726fe90a34020",
+    "url": "css/city.2a3a06c5.css"
+  },
+  {
+    "revision": "6c5fb9e4e4ff0fbed120",
+    "url": "css/country.2a3a06c5.css"
+  },
+  {
+    "revision": "f73fa14fa90e9474919d",
+    "url": "css/feature.2a3a06c5.css"
+  },
+  {
+    "revision": "5e2d2aeeec1e3592a3fd",
+    "url": "css/news.2a3a06c5.css"
+  },
+  {
+    "revision": "efd26759323926b9c305",
+    "url": "css/newscarousel.2a3a06c5.css"
+  },
+  {
+    "revision": "86cccdae3d528059d1d3",
+    "url": "css/register.2a3a06c5.css"
+  },
+  {
+    "revision": "889c4371474eea05348ccd3d1419d254",
+    "url": "genshin_s.jpg"
+  },
+  {
+    "revision": "1939a8f0f7d97546ac1e41d802f4d7f9",
+    "url": "img/bg.1939a8f0.jpg"
+  },
+  {
+    "revision": "0e1108c0fece80e2b8b787f17e60651b",
+    "url": "img/char2.0e1108c0.png"
+  },
+  {
+    "revision": "d38589efa4fe0a1f5c91261ea70295fb",
+    "url": "img/char_sbg_hover.d38589ef.png"
+  },
+  {
+    "revision": "6c30b2423f560ae9464def3561fa1931",
+    "url": "img/charater.6c30b242.png"
+  },
+  {
+    "revision": "da6bd4d3dbad7965416188da00f73f70",
+    "url": "img/featrue_bg.da6bd4d3.png"
+  },
+  {
+    "revision": "9a384d00c0508ade163f721d8e43024a",
+    "url": "img/feature.9a384d00.png"
+  },
+  {
+    "revision": "1f349360c183505a3fb72be9d9d3ccaf",
+    "url": "img/footer.1f349360.png"
+  },
+  {
+    "revision": "30c26df91730f61a0e59997d36820360",
+    "url": "img/footer_img.30c26df9.png"
+  },
+  {
+    "revision": "5f8e397e1fb579d3c07056579759b6fa",
+    "url": "img/footer_name.5f8e397e.png"
+  },
+  {
+    "revision": "81e21554c880605682ce0f40e5a5c5d3",
+    "url": "img/genshin.81e21554.png"
+  },
+  {
+    "revision": "69d569ae4f38320d1595104e450d1921",
+    "url": "img/home.69d569ae.png"
+  },
+  {
+    "revision": "a5b5d4166c094c433226b2d93077cab6",
+    "url": "img/iconfont.a5b5d416.svg"
+  },
+  {
+    "revision": "9c210fd374a89e5775ef00e252fac0e3",
+    "url": "img/instagram.9c210fd3.png"
+  },
+  {
+    "revision": "b85e79b296ba282c00a25c35e5c32969",
+    "url": "img/lock.b85e79b2.png"
+  },
+  {
+    "revision": "075826efccde86042bb808ae5bb9e954",
+    "url": "img/m_character.075826ef.png"
+  },
+  {
+    "revision": "be3850276e43d821789fa570ffd96d99",
+    "url": "img/m_feature.be385027.png"
+  },
+  {
+    "revision": "fb2d06024f3b99988d6d38c1c5617297",
+    "url": "img/m_home.fb2d0602.jpg"
+  },
+  {
+    "revision": "76dca2aa7499747748583cb3bf18df8d",
+    "url": "img/m_new.76dca2aa.png"
+  },
+  {
+    "revision": "a07e5568cee62be71fa531910430f058",
+    "url": "img/m_world.a07e5568.png"
+  },
+  {
+    "revision": "8cf02deb259bf77cdc30fb44471e9520",
+    "url": "img/navbar_logo.8cf02deb.png"
+  },
+  {
+    "revision": "f68a7bf5ac9b6065d3968b5e92167334",
+    "url": "img/news.f68a7bf5.png"
+  },
+  {
+    "revision": "21392ab820a2604cff63098f331c6833",
+    "url": "img/sakura.21392ab8.png"
+  },
+  {
+    "revision": "0b974c7e01faf7d78da5931c589ba72f",
+    "url": "img/sakura1.0b974c7e.png"
+  },
+  {
+    "revision": "71c7c5137f3040f4cce3903d3870dc18",
+    "url": "img/sidebar-6.71c7c513.jpg"
+  },
+  {
+    "revision": "fd4b875929ede9713181d108031d609d",
+    "url": "img/user.fd4b8759.png"
+  },
+  {
+    "revision": "1c493871d378caaf149d12120c4c4adb",
+    "url": "img/user_black.1c493871.png"
+  },
+  {
+    "revision": "47f71d48d19bef20e76c5ada47bbed04",
+    "url": "img/videobg.47f71d48.jpg"
+  },
+  {
+    "revision": "622fce2a8a3327c81b62688cf2e49bd5",
+    "url": "img/world.622fce2a.png"
+  },
+  {
+    "revision": "5316b4bdc49e97267593e62a7478f503",
+    "url": "img/world_title.5316b4bd.png"
+  },
+  {
+    "revision": "d8e947e0c661d28efd819f7919918547",
+    "url": "index.html"
+  },
+  {
+    "revision": "eb2510a21eaa619aa51a",
+    "url": "js/Attribute.35acf782.js"
+  },
+  {
+    "revision": "0fd170081c9de735a537",
+    "url": "js/BackAccount.322e2a2d.js"
+  },
+  {
+    "revision": "a67df409837b5b2657d5",
+    "url": "js/admin.7e13a3e8.js"
+  },
+  {
+    "revision": "fddc19183e96a277c660",
+    "url": "js/app.f803da57.js"
+  },
+  {
+    "revision": "8e5d48cbc9e66b0a4375",
+    "url": "js/character.d02db6ad.js"
+  },
+  {
+    "revision": "5840554d5e90781efd44",
+    "url": "js/chunk-vendors.e1f7b2a5.js"
+  },
+  {
+    "revision": "36a19b0726fe90a34020",
+    "url": "js/city.da579eb6.js"
+  },
+  {
+    "revision": "6c5fb9e4e4ff0fbed120",
+    "url": "js/country.30327536.js"
+  },
+  {
+    "revision": "f73fa14fa90e9474919d",
+    "url": "js/feature.99e42ce0.js"
+  },
+  {
+    "revision": "e63654cbdf163b7fb83c",
+    "url": "js/login.e0c7087e.js"
+  },
+  {
+    "revision": "5e2d2aeeec1e3592a3fd",
+    "url": "js/news.234fad25.js"
+  },
+  {
+    "revision": "efd26759323926b9c305",
+    "url": "js/newscarousel.acac8c72.js"
+  },
+  {
+    "revision": "86cccdae3d528059d1d3",
+    "url": "js/register.569d5182.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "3e78e808a274ec2caf5dce50012d62ad",
+    "url": "media/bg.3e78e808.mp4"
+  },
+  {
+    "revision": "2f4f7b95e1d8d0318d41cc7016174b07",
+    "url": "media/m_bg.2f4f7b95.mp4"
+  }
+]);
